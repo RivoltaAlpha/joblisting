c486c05aa7af664dbcf0cb8237293d4a8cdc1b1c
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-06-01 10:04:46 +0300

    filter roles and css styling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tags = document.querySelectorAll(".tags span");
   const jobCards = document.querySelectorAll(".job-card");
-  const searchBox = document.getElementById('searchBox');
-  const backButton = document.getElementById('backButton');
+  const searchBox = document.getElementById("searchBox");
+  const backButton = document.getElementById("backButton");
 
   tags.forEach((tag) => {
     tag.addEventListener("click", function () {
@@ -25,35 +25,42 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-  searchBox.addEventListener('input', filterJobs);
+  searchBox.addEventListener("input", filterJobs);
 
   function filterJobs() {
-      const filter = searchBox.value.trim().toLowerCase();
-      let filtered = false;
+    const filter = searchBox.value.trim().toLowerCase();
+    let filtered = false;
 
-      jobCards.forEach(card => {
-          const role = card.getAttribute('data-role').toLowerCase();
-          const languages = card.getAttribute('data-languages').toLowerCase();
+    jobCards.forEach((card) => {
+      const role = card.getAttribute("data-role").toLowerCase();
+      const level = card.getAttribute("data-level").toLowerCase();
+      const languages = card.getAttribute("data-languages").toLowerCase();
+      const tools = card.getAttribute("data-tools").toLowerCase();
 
-          if (role.includes(filter) || languages.includes(filter)) {
-              card.style.display = '';
-              filtered = true;
-          } else {
-              card.style.display = 'none';
-          }
-      });
-
-      // Toggle back button visibility
-      if (filtered) {
-          backButton.style.display = 'inline-block';
+      if (
+        role.includes(filter) ||
+        level.includes(filter) ||
+        languages.includes(filter) ||
+        tools.includes(filter)
+      ) {
+        card.style.display = "";
+        filtered = true;
       } else {
-          backButton.style.display = 'none';
+        card.style.display = "none";
       }
+    });
+
+    // Toggle back button visibility
+    if (filtered) {
+      backButton.style.display = "inline-block";
+    } else {
+      backButton.style.display = "none";
+    }
   }
 
-  backButton.addEventListener('click', function () {
-      searchBox.value = '';
-      filterJobs();
-      window.location.reload();
+  backButton.addEventListener("click", function () {
+    searchBox.value = "";
+    filterJobs();
+    window.location.reload();
   });
 });
